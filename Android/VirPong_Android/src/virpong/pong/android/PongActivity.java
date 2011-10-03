64f744b4eb001a5d4c6c55654b418490e47d0c13
Kyle Wenholz <kylewenholz@gmail.com>
2011-10-03 16:50:48 -0700

    modifying some source, deleting old versions, and cleaning out the test server

--- Android/VirPong_Android/src/virpong/pong/android/PongActivity.java
+++ Android/VirPong_Android/src/virpong/pong/android/PongActivity.java
@@ -5,7 +5,6 @@ import com.phonegap.*;
 
 public class PongActivity extends DroidGap {
     /** Called when the activity is first created. */
-    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.loadUrl("file:///android_asset/www/index.html");
