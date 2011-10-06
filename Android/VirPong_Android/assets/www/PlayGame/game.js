33e0595c0625ddcc2d28a1741034fb73a6d5044e
Kyle Wenholz <kylewenholz@gmail.com>
2011-10-06 15:51:43 -0700

    Made text area in the main app.

--- Android/VirPong_Android/assets/www/PlayGame/game.js
+++ Android/VirPong_Android/assets/www/PlayGame/game.js
@@ -1,4 +1,6 @@
- function promptLogin(){
+
+
+function promptLogin(){
 		var name = prompt("Username please. (Use \"guest\" if you don't already have an account.");
 		var pass = prompt("Please enter your password. (If you are logging in as \"guest\" then please use \"pass\".)");
 }
