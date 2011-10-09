6778d7509f2ce436e0bf0b3a35a862635c0ab854
Kyle Wenholz <kylewenholz@gmail.com>
2011-10-09 16:18:57 -0700

    CLEAN STATE: There is no test code here.  The project simply displays a main page and game page along with the ability to crudely capture wiimote data.

--- Android/VirPong_Android/src/virpong/pong/android/PongActivity.java
+++ Android/VirPong_Android/src/virpong/pong/android/PongActivity.java
@@ -2,14 +2,11 @@ package virpong.pong.android;
 
 import android.os.Bundle;
 import com.phonegap.*;
-import com.strumsoft.websocket.phonegap.WebSocketFactory;
 
 public class PongActivity extends DroidGap {
     /** Called when the activity is first created. */
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.loadUrl("file:///android_asset/www/index.html");
-        // attach websocket factory
-		appView.addJavascriptInterface(new WebSocketFactory(appView), "WebSocketFactory");
     }
 }
