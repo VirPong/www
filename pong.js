752dd321527c41cffae358ed331d45caf04706c8
Josef Lange <josef.d.lange@gmail.com>
2011-12-13 01:48:43 -0800

    Got rid of scroll protection and fixed local Accelerometer code. Also some other fixes, including dropping sound.

--- pong.js
+++ pong.js
@@ -5,6 +5,7 @@
 
 //Local accel.
 var watchID;
+var updateFrequencyCounter = 0;
 
 //Username and password.
 var name;
@@ -204,13 +205,13 @@ function eventController(eventCode){
  * Performs actions related to the wall bounce event.
  */
 function wallBounceEvent(){
-    wallBounceSound.play();   
+    //wallBounceSound.play();   
 };
 /**
  * Performs actions related to the paddle bounce event.
  */
 function paddleBounceEvent(){
-    paddleBounceSound.play();
+   // paddleBounceSound.play();
 };
 
 
@@ -456,13 +457,11 @@ function displaySelection(selection, options){
 	    options;
     }else if(selection == "newRoom"){
 	view.innerHTML = " <div id=\"mainWrapper\"><h1 align=\"center\">What do you want to name "+
-	    "your game room?</h1><form name=\"roomName\" "+
-	    "id=\"pinEntry\"><!-- Room --><input type=\"text\" "+
+	    "your game room?</h1><!-- Room --><input type=\"text\" "+
 	    "value=\"\" name=\"userName\" id=\"roomName\" "+
 	    "onFocus=\"this.value=\"\"\" autocapitalize=\"off\" "+
 	    "autocorrect=\"off\" /><!-- Submit --><input type=\"button\" "+
-	    "value=\"Select Room\" id=\"selectRoom\" "+
-	    "onClick=\"handleNewRoom();\" /></form><br /></div>";
+	    "value=\"Select Room\" id=\"selectRoom\" onClick=\"handleNewRoom()\" /></form><br /></div>";
     }else if(selection == "gameEnd"){
 	view.innerHTML == "The game is over.  You "+options+".";
     }
@@ -474,14 +473,14 @@ function handleRoomSelect(){
 };
 
 function handleNewRoom(){
-    var room = document.roomName.roomName.value;
+    var room = document.getElementById('roomName').value;
     createRoom(room);
 };
 
 function setupLocalAccelerometer() {
   
     // Set the frequency of refresh for Accelerometer data.
-    var options = { frequency: 50 };
+    var options = { frequency: 200 };
     
     // Start watching accerlation and point to it with watchID.
     watchID = navigator.accelerometer.watchAcceleration(onSuccess,
@@ -499,10 +498,16 @@ function setupLocalAccelerometer() {
  */
 function onSuccess(acceleration)
 { 
-    // Get paddlePosition from getPosition, round it to an integer
-    // scale it up and add the "px" to it so it can be injected
-    // into CSS styling for data visualization in paddle form.
-    updatePaddleToServer(Math.round(getPosition(acceleration)));
+
+    if(acceleration.x<-.2) {
+        
+        changePaddlePosition("S");
+        
+    } else if(acceleration.x>.2) {
+        
+        changePaddlePosition("W");
+        
+    }
     
 }
 
@@ -512,81 +517,5 @@ function onSuccess(acceleration)
 function onError(acceleration) {
     
     alert('Error!');
-    
-};
-
-// Setup for calculating position.
-// These are basically a bunch of initial values.
-// Their nomenclature makes clear their purpose.
-var currentAcceleration = 0;
-var currentVelocity = 0;
-var currentPosition = 50;
-
-var previousAcceleration = 0;
-var previousVelocity = 0;
-var previousPosition = 0;
-
-// What to use as a frequency to calculate passing
-// of time between accelerometer data refreshes.
-var TIME = .3;
-
-/*
- * Calculates the position of the "paddle" given accelerometer data from tilt-action.
- * @param   acceleration    The acceleration data (x,y,z,timestamp).
- * @return  int position    An integer from 0 to 100 representing position.
- */
-function getPosition(acceleration) {
-    
-    // Invert acceleration for the sake of mapping the tilts
-    // in the correct direction.
-    currentAcceleration = -1*acceleration.y;
-    
-    // Double the acceleration for more usable data.
-    var virtualAcceleration = 2*currentAcceleration;
-    
-    // This detects if acceleration is in the opposite direction
-    // of velocity, such as when we're changing direction.
-    if((currentVelocity<0&&currentAcceleration>0) || (currentVelocity>0&&currentAcceleration<0)) {
-        
-        // Instead of doubling the acceleration, quadruple it.
-        // This makes the deceleration and change of direction
-        // happen much more quickly.
-        virtualAcceleration = currentAcceleration * 4;
-        
-    }
-    
-    // Add TIME * virtualAcceleration to the previous Velocity
-    // to update it to what is close to what the current Velocity
-    // would be.
-    currentVelocity = previousVelocity + (TIME * virtualAcceleration);
-    
-    // Find the midpoint between previous and current.
-    var averageVelocity = ((currentVelocity) + (previousVelocity)) / 2;
-    
-    // Just as we did with the Velocity from Acceleration, do to Position
-    // from Velocity.
-    currentPosition = currentPosition + (TIME * averageVelocity);
-    previousPosition = currentPosition;
-    
-    // Set the currents to the previouses to prepare for the next cycle through this function.
-    previousVelocity = currentVelocity;
-    previousAcceleration = currentAcceleration;
-    
-    // Prevent us from going over 100 or under 0.
-    if(currentPosition>100) { currentPosition = 100; }
-    if(currentPosition<0) { currentPosition = 0; }
-    
-    // If we're at either end of the range, set acceleration and velocity
-    // to zero so that we immediately change direction.
-    if(currentPosition==0||currentPosition==100) 
-    { 
-        previousVelocity = 0;
-        previousAcceleration = 0;
-        currentVelocity = 0;
-        currentAcceleration = 0; 
-    }
-    
-    // Return our calculated position. 
-    return currentPosition;
     
 };
